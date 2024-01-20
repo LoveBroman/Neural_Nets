Some problems with backprop fixed

layers.py
@@ -11,20 +11,29 @@ class Dense(Layer):
     def __init__(self, insize, outsize, act="linear"):
         self.insize = insize
         #self.input = input
-        self.W = np.random.rand(self.insize, outsize)
+        self.W = np.random.rand(self.insize, outsize).T
         self.bias = np.random.rand()
         self.act = act
 
     def forward(self, input):
-        a = np.matmul(self.W, input)
+        if isinstance(input, float) or isinstance(input, int):
+            a = input * self.W
+        else:
+            a = self.W @ input
         if self.act == "linear":
             self.out = a
         elif self.act == "sigmoid":
             self.out = sigmoid(a)
         elif self.act == "tanh":
-            self.out =  np.tanh(a)
+            self.out = np.tanh(a)
         elif self.act == "relu":
             self.out = relu(a)
+        return self
 
+class Input(Layer):
+    def __init__(self, insize):
+        self.insize = insize
 
-
+    def forward(self, input):
+        self.out = input
+        return self

network.py
@@ -1,46 +1,72 @@
 import numpy as np
 from layers import *
+from utils import *
 
 class NeuralNet:
-     def __init__(self, layers=None):
-         if layers is not None:
-             self.layers = layers
-         else:
-             self.layers = []
+    def __init__(self, a, epochs, batch_size=None, layers=None):
+        self.a = a
+        self.epochs = epochs
+        self.batch_size = batch_size
+        if layers is not None:
+            self.layers = layers
+        else:
+            self.layers = []
+    def add_layer(self, layer):
+        self.layers.append(layer)
+    def for_prop(self, input):
+        nextout = input
+        for layer in self.layers:
+            nextout = layer.forward(nextout).out
+        return nextout
 
-     def add_layer(self, layer):
-         self.layers.append(layer)
+    def predict(self, X):
+        pred = np.vectorize(self.for_prop)
+        return pred(X)
 
-     def for_prop(self, input):
-         nextout = input
-         for layer in self.layers:
-             nextout = layer.forward(nextout).out
-         return nextout
+    def back_prop(self, batch, y):
+        deltas = [None] * (len(self.layers) - 1)
+        w_deltas = [None] * (len(self.layers) - 1)
+        for i in range(len(self.layers))[:-1]:
+            deltas[i] = np.zeros(self.layers[i+1].W.shape[0])
+            w_deltas[i] = np.zeros(self.layers[i+1].W.shape)
+        for i, x in enumerate(batch):
+            self.for_prop(x)
+            for j in list(reversed(range(len(self.layers))))[:-1]:
+                if j == len(self.layers) - 1:
+                    eo = y[i] - self.layers[j].out
+                    dact = np.vectorize(derivate(self.layers[j].act))
+                    deltas[j-1] = dact(self.layers[j].out) * eo
+                else:
+                    dact = np.vectorize(derivate(self.layers[j].act))
+                    eo = dact(self.layers[j].out)
+                    deltas[j-1] = eo * np.matmul(self.layers[j].W, deltas[j].T)
+
+            for j in range(len(deltas)):
+                if isinstance(self.layers[j].out, int) or isinstance(self.layers[j].out, np.float64):
+                    w_deltas[j] += deltas[j].reshape(len(deltas[j]), 1) * self.layers[j].out
+                else:
+                    if j == 2:
+                        pass
+                    print(f"j is {j}")
+                    w_deltas[j] += deltas[j] @ self.layers[j].out.T
+        for i in range(len(self.layers))[1:]:
+            if i == 2:
+                pass
+            self.layers[i].W += w_deltas[i - 1]
 
 
-     def back_prop(self, batch, y):
-         w_delt = []
-         deltas = [] * len(self.layers)
-         for i, l in enumerate(self.layers):
-             deltas[i] = np.zeros(l.W.shape)
-         os = []
-         for x, i in enumerate(batch):
-             self.for_prop(x)
-             for j in reversed(range(len(self.layers)))[:-1]:
-                 if j == len(self.layers) -1:
-                     eo = y[i] - self.layers[j].out
-                     deltas[j] = self.layers[j - 1].out * eo
-                 else:
-                     dact = np.vectorize(derivate(self.layers[j].act))
-                     eo = dact(self.layers[j].out)
-                     dw = self.layers[j -1] @ eo.reshape(1, len(eo))
-                     deltas[j] = dw
-         for i, d in enumerate(deltas):
-             self.layers[i] -= self.a * d
+    def get_batch(self, X, y):
+        rg = RandGen(len(y))
+        binds = rg.getk(self.batch_size)
+        xbatch, ybatch = X[binds], y[binds]
+        X, y = np.delete(X, binds, 0), np.delete(y, binds, 0)
+        return xbatch, ybatch, X, y
 
-     def fit(self, X, y):
-        for i in range(len(X)):
-            yhat = self.for_prop(X[i])
-            self.back_prop(yhat, y)
-
-
+    def fit(self, X, y):
+        for i in range(self.epochs):
+            if self.batch_size is None:
+                self.back_prop(X, y)
+            else:
+                while len(y) != 0:
+                    Xbatch, ybatch, X, y = self.get_batch(X, y)
+                    self.back_prop(Xbatch, ybatch)