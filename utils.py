Some problems with backprop fixed

utils.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-def relu(a):
+def _relu(a):
     return max(0,a)
 
-def sigmoid(a):
+def relu(a):
+    return np.vectorize(_relu)(a)
+
+def _sigmoid(a):
     return 1/(1+np.exp(-a))
+
+def sigmoid(a):
+    return np.vectorize(_sigmoid)(a)
 
 def mse(y, yhat):
     return sum ((y - yhat) ** 2)
@@ -13,13 +19,15 @@ def derivate(fs):
     if fs == "relu":
         return lambda x: int(x > 0)
     elif fs == "sigmoid":
-        return lambda x: sigmoid(x) (1 - sigmoid(x))
+        return lambda x: x * (1 - x)
+    elif fs == "linear":
+        return lambda x: x
     else:
-        return lambda x: (fs(x + 1e-7) - fs(x))/ 1e-7
+        return lambda x: (fs(x + 1e-7) - fs(x)) / 1e-7
 
 
 #Generates randomnumbers a different one each time.
-class RandomGen:
+class RandGen:
     def __init__(self, n):
         self.n = n
         self.nums = []